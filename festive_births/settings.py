7e5b5af9de6a7138ca1b44d414e34c2bd50ef76a
Vernon Mosterd <vernon.mosterd@gmail.com>
2025-10-31 19:33:30 +0200

    Coolify Deploy

--- festive_births/settings.py
+++ festive_births/settings.py
@@ -11,24 +11,33 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # ==========================================================
 # CORE DEPLOYMENT SETTINGS
 # ==========================================================
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback-secret-key')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+# Add your domain/IP from Coolify
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+
+# Trust Coolify internal routing and domain
 CSRF_TRUSTED_ORIGINS = []
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-    CSRF_TRUSTED_ORIGINS.append(f'https://{RENDER_EXTERNAL_HOSTNAME}')
+COOLIFY_DOMAIN = os.environ.get('COOLIFY_DOMAIN')
+if COOLIFY_DOMAIN:
+    CSRF_TRUSTED_ORIGINS.append(f'https://{COOLIFY_DOMAIN}')
+    ALLOWED_HOSTS.append(COOLIFY_DOMAIN)
 
 # ==========================================================
 # APPLICATION DEFINITION
 # ==========================================================
 INSTALLED_APPS = [
-    'django.contrib.admin', 'django.contrib.auth', 'django.contrib.contenttypes',
-    'django.contrib.sessions', 'django.contrib.messages', 'django.contrib.staticfiles',
-    'births.apps.BirthsConfig', 'accounts.apps.AccountsConfig',
-    'crispy_forms', 'crispy_bootstrap5',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'births.apps.BirthsConfig',
+    'accounts.apps.AccountsConfig',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -48,11 +57,14 @@ ROOT_URLCONF = 'festive_births.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'], 'APP_DIRS': True,
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug', 'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth', 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
@@ -61,21 +73,41 @@ TEMPLATES = [
 WSGI_APPLICATION = 'festive_births.wsgi.application'
 
 # ==========================================================
-# DATABASE & CACHE
+# DATABASE CONFIGURATION
 # ==========================================================
 DATABASE_URL = os.environ.get('DATABASE_URL')
-IS_PRODUCTION = 'RENDER' in os.environ
 
 if DATABASE_URL:
-    DATABASES = {'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600, ssl_require=IS_PRODUCTION)}
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=False  # Coolify local SSL handled by reverse proxy
+        )
+    }
 else:
-    DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3', 'NAME': BASE_DIR / 'db.sqlite3'}}
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3'
+        }
+    }
 
 # ==========================================================
 # AUTHENTICATION & SESSION MANAGEMENT
 # ==========================================================
-AUTHENTICATION_BACKENDS = ['accounts.backends.PersalAuthBackend', 'django.contrib.auth.backends.ModelBackend']
-AUTH_PASSWORD_VALIDATORS = [{'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'}, {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'}, {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'}, {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'}]
+AUTHENTICATION_BACKENDS = [
+    'accounts.backends.PersalAuthBackend',
+    'django.contrib.auth.backends.ModelBackend'
+]
+
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+]
+
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'delivery_list'
 LOGOUT_REDIRECT_URL = 'landing_page'
@@ -85,22 +117,27 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 # INTERNATIONALIZATION & STATIC FILES
 # ==========================================================
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Johannesburg'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # ==========================================================
-# EMAIL
+# EMAIL CONFIGURATION
 # ==========================================================
 EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND', 'django.core.mail.backends.console.EmailBackend')
+EMAIL_HOST = os.environ.get('EMAIL_HOST', '')
+EMAIL_PORT = os.environ.get('EMAIL_PORT', 587)
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 
 # ==========================================================
-# MISC / 3rd PARTY SETTINGS
+# MISC / 3RD PARTY SETTINGS
 # ==========================================================
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
@@ -113,21 +150,12 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'formatters': {
-        'verbose': {
-            'format': '{levelname} {asctime} {module} {message}',
-            'style': '{',
-        },
+        'verbose': {'format': '{levelname} {asctime} {module} {message}', 'style': '{'},
     },
     'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose',
-        },
+        'console': {'class': 'logging.StreamHandler', 'formatter': 'verbose'},
     },
-    'root': {
-        'handlers': ['console'],
-        'level': 'INFO',
-    },
+    'root': {'handlers': ['console'], 'level': 'INFO'},
     'loggers': {
         'django': {
             'handlers': ['console'],
