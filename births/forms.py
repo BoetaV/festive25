30596590d92262d88c9c9d67af1bd2b9bd514497
Vernon Mosterd <vernon.mosterd@gmail.com>
2025-12-31 20:10:50 +0200

    Remove outdated report date from choices

--- births/forms.py
+++ births/forms.py
@@ -14,7 +14,7 @@ FACILITY_TYPE_CHOICES = [
     ("Private Hospital", "Private Hospital"), ("Regional Hospital", "Regional Hospital"), 
     ("Tertiary Hospital", "Tertiary Hospital"),
 ]
-REPORT_DATE_CHOICES = [("", "--Select Report Date--"), ("25 December 2025", "25 December 2025"), ("01 January 2026", "01 January 2026")]
+REPORT_DATE_CHOICES = [("", "--Select Report Date--"), ("01 January 2026", "01 January 2026")]
 TIME_SLOT_CHOICES = [("", "--Select Time Slot--"), ("00:01 - 06:00", "00:01 - 06:00"), ("06:01 - 12:00", "06:01 - 12:00"), ("12:01 - 18:00", "12:01 - 18:00"), ("18:01 - 24:00", "18:01 - 24:00")]
 BIRTH_MODE_CHOICES = [("", "--Select Birth Mode--"), ("Normal Vertex", "Normal Vertex"), ("Caesarean section Elective", "Caesarean section Elective"), ("Caesarean section Emergency", "Caesarean section Emergency"), ("Vacuum", "Vacuum"), ("Forceps", "Forceps"), ("Vaginal Breech", "Vaginal Breech")]
 
